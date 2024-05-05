692349e8db73b0d1be32bc525a4c3a1708354ec8
anshuER <anshupatel710@gmail.com>
2024-05-05 18:29:58 +0530

    Scroll

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -20,8 +22,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -31,3 +32,56 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+  const s1coords = section1.getBoundingClientRect();
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// querySelector
+// getElementById
+// const header = document.querySelector('.header');
+
+// return HTMLELEMENTS
+// getElementByTagName like button and all that it updates automaticcaly when dom update.
+// getELementByClassName
+
+// Creating ELement
+// // .insertAdjacent
+// // createElement return DOM element
+// const msg = document.createElement('div');
+// msg.classList.add('cookie-message');
+// msg.innerHTML = 'Demo<button class="btn btn--close--cookie">Got It</button>';
+
+// // prepend
+// header.append(msg);
+// // before and after
+
+// // delete element
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener('click', function () {
+//     msg.remove();
+//   });
+
+// // styles
+// msg.style.backgroundColor = '#37383d';
+// msg.style.width = '120%';
+
+// msg.style.height =
+//   Number.parseFloat(getComputedStyle(msg).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
